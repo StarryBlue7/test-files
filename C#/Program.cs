6354aa93e36ab78b315aeceaa3f8ee0bf1acb79c
Vince <starryblue7@gmail.com>
2022-12-08 10:43:23 -0800

    add library, diagnostic, & file/array manipulation

--- C#/Program.cs
+++ C#/Program.cs
@@ -155,12 +155,237 @@
 // }
 // Console.WriteLine($"We have {sum} items in inventory.");
 
-string[] orderIds = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
+//----------------------------------------------------------------------------------------------------------------------
 
-foreach (string orderId in orderIds) 
+// string[] orderIds = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
+
+// foreach (string orderId in orderIds) 
+// {
+//   if (orderId.StartsWith("B")) 
+//   {
+//     Console.WriteLine(orderId);
+//   }
+// }
+
+// using Humanizer;
+
+// Console.WriteLine("Quantities:");
+// HumanizeQuantities();
+
+// Console.WriteLine("\nDate/Time Manipulation:");
+// HumanizeDates();
+
+// static void HumanizeQuantities()
+// {
+//     Console.WriteLine("case".ToQuantity(0));
+//     Console.WriteLine("case".ToQuantity(1));
+//     Console.WriteLine("case".ToQuantity(5));
+// }
+
+// static void HumanizeDates()
+// {
+//     Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+//     Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+//     Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+//     Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+// }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// using System.Diagnostics;
+
+// int result = Fibonacci(5);
+// Console.WriteLine(result);
+
+// static int Fibonacci(int n)
+// {
+//     Debug.Assert(n >= 0, $"{n} is negative and will cause failure.");
+//     Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+//     Debug.WriteLine($"We are looking for the {n}th number");
+    
+//     int n1 = 0;
+//     int n2 = 1;
+//     int sum;
+
+//     for (int i = 2; i <= n; i++)
+//     {
+//         sum = n1 + n2;
+//         n1 = n2;
+//         n2 = sum;
+//         Debug.WriteLine("loop "  + i);
+//         Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");    
+//     }
+
+//     return n == 0 ? n1 : n2;
+// }
+
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+// var salesFiles = FindFiles("stores");
+
+// foreach (var file in salesFiles)
+// {
+//     Console.WriteLine(file);
+// }
+
+// IEnumerable<string> FindFiles(string folderName)
+// {
+//   List<string> salesFiles = new List<string>();
+
+//   var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+//   foreach (var file in foundFiles)
+//   {
+//     // The file name will contain the full path, so only check the end of it
+//     // if (file.EndsWith("sales.json"))
+//     // {
+//     //     salesFiles.Add(file);
+//     // }
+//     // var extension = Path.GetExtension(file);
+//     var extension = Path.GetExtension(file);
+//     if (extension == ".json")
+//     {
+//         salesFiles.Add(file);
+//     }
+//   }
+
+//   return salesFiles;
+// }
+
+// string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+// Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+// Console.WriteLine(Path.Combine("stores","201"));
+// returns:
+// stores\201 on Windows
+// stores/201 on macOS
+
+// string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+
+// FileInfo info = new FileInfo(fileName);
+
+// Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
+
+// var currentDirectory = Directory.GetCurrentDirectory();
+// var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+// var salesFiles = FindFiles(storesDirectory);
+
+// foreach (var file in salesFiles)
+// {
+//   Console.WriteLine(file);
+// }
+
+// Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores","201","newDir"));
+
+// bool doesDirectoryExist = Directory.Exists("filePath");
+
+// File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "greeting.txt"), "Hello World!");
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Creating directories/files
+
+// var currentDirectory = Directory.GetCurrentDirectory();
+// var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+// var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+// Directory.CreateDirectory(salesTotalDir); 
+
+// var salesFiles = FindFiles(storesDirectory);
+
+// File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Read/write files
+
+// var currentDirectory = Directory.GetCurrentDirectory();
+// var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+// var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+// Directory.CreateDirectory(salesTotalDir);   
+
+// var salesFiles = FindFiles(storesDirectory);
+
+// var salesTotal = CalculateSalesTotal(salesFiles);
+
+// File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+// IEnumerable<string> FindFiles(string folderName)
+// {
+//     List<string> salesFiles = new List<string>();
+
+//     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+//     foreach (var file in foundFiles)
+//     {
+//         var extension = Path.GetExtension(file);
+//         if (extension == ".json")
+//         {
+//             salesFiles.Add(file);
+//         }
+//     }
+
+//     return salesFiles;
+// }
+
+// double CalculateSalesTotal(IEnumerable<string> salesFiles)
+// {
+//     double salesTotal = 0;
+    
+//     // Loop over each file path in salesFiles
+//     foreach (var file in salesFiles)
+//     {      
+//         // Read the contents of the file
+//         string salesJson = File.ReadAllText(file);
+    
+//         // Parse the contents as JSON
+//         SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+    
+//         // Add the amount found in the Total field to the salesTotal variable
+//         salesTotal += data?.Total ?? 0;
+//     }
+    
+//     return salesTotal;
+// }
+
+// record SalesData (double Total);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Reverse words in string
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+
+// string[] split = pangram.Split(' ');
+// string[] reversedSplit = new string[split.Length];
+
+// for (int i = 0; i < split.Length; i++)
+// {
+//     char[] letters = split[i].ToCharArray();
+//     Array.Reverse(letters);
+//     reversedSplit[i] = new string(letters);
+// }
+
+// string reversed = String.Join(" ", reversedSplit);
+
+// Console.WriteLine(reversed);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Filter
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orders = orderStream.Split(',');
+
+foreach (string order in orders) 
 {
-  if (orderId.StartsWith("B")) 
-  {
-    Console.WriteLine(orderId);
-  }
+    if (order.StartsWith("B"))
+    {
+        Console.WriteLine(order);
+    }
 }
